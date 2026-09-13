68ac651fab9a5d9f076d6ece3e947ffab28bd1d6
habibtegar <habibtegartegarramadhan@gmail.com>
2026-09-13 15:05:25 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -88,7 +88,7 @@
             <div class="auth-header">
                 <span class="auth-icon">📖</span>
                 <h1 class="auth-title">Masuk ke Perpustakaan</h1>
-                <p class="auth-subtitle">Gunakan akun Admin atau</p>
+                <p class="auth-subtitle">Gunakan akun Admin atau User</p>
             </div>
 
             @if (session('success'))
